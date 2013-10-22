460a2c25d515886923a034f321b9c30a5d3a244c
OldPartizan <redpatizan@gmail.com>
2013-10-22 15:47:00 +0300

    chat add

---
+++ js/login.js
@@ -0,0 +1,13 @@
+﻿var login = null;
+
+function show(){
+    var username = prompt("Для входа в чат просто введите ваше имя","");
+    if (username != null && username!=""){
+       login = username;
+       user_conect ();
+
+    }
+    else {
+    document.write("Ошибка! Попробуте еще раз.");
+    }
+}
---
+++ js/mes.js
@@ -0,0 +1,53 @@
+﻿var socket = io.connect('http://localhost');
+
+
+
+function user_conect () {
+socket.emit('user', login);
+socket.on('conect', function (data) {
+		addMessage(data);
+});
+
+}
+
+
+
+function addMessage(data) {
+        var divr = document.createElement('b');
+		divr.innerHTML = data;
+		$(divr).insertBefore('#mess');
+		$('#mess').insertBefore(divr);
+    }
+
+
+
+socket.on ('addconect', function (data) {
+  		
+		
+        adduser (data);
+ 
+});
+
+
+function m () { 
+	
+		
+	$('#msg_input').click(function() {
+		var x = $('#messages').val();
+		socket.emit('my_event',  login + ': ' + x);
+		$('#messages').val('');
+	});	
+  	
+	socket.on('mmm', function (data) {
+		
+		addMessage(data);
+
+				
+		
+	});
+
+}
+
+
+	
+	
